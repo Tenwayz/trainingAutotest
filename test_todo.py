6afce962735eecec9b635d3f699db19b7b128317
Tenwayz <dimazzz789@gmail.com>
2025-04-07 20:11:41 +0700

    add page

---
+++ test_todo.py
@@ -0,0 +1,9 @@
+import re, pytest
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+def test_add_todo(page):
+    page.goto("https://demo.playwright.dev/todomvc/#/")
+    page.get_by_role("textbox", name="What needs to be done?").click()
+    page.get_by_role("textbox", name="What needs to be done?").fill("Teeeeext")
+    page.get_by_role("textbox", name="What needs to be done?").press("Enter")
+    page.get_by_role("checkbox", name="Toggle Todo").check()
