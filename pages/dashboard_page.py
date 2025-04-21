a06ccaeae318436562843a81c59862b7710a1638
Tenwayz <dimazzz789@gmail.com>
2025-04-21 20:28:55 +0700

    add dashboard_page

---
+++ pages/dashboard_page.py
@@ -0,0 +1,14 @@
+from playwright.sync_api import Page, expect
+
+
+class DashboardPage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.profile = page.locator('#usernameDisplay')
+        self.logout = page.locator('#logout')
+
+    def print_message(self):
+        print(self.profile.inner_text())
+
+    def assert_welcome_message(self, message):
+        expect(self.profile).to_have_text(message)
